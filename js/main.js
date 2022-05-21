c217c3af86c88a789950f0a859d5a62035269fbc
Maxime Lombard <maxime.lombard@live.com.mx>
2022-05-21 16:43:37 +0200

    mainly finished the portfolio

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// SLIDER START
+
 let counter = 1;
 setInterval(function() {
     document.getElementById('radio' + counter).checked = true;
@@ -6,3 +8,19 @@ setInterval(function() {
         counter = 1
     }
 }, 5000);
+
+// SLIDER END
+
+// TO THE TOP START
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+    if(window.pageYOffset > 100) {
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active");
+    }
+})
+
+// TO THE TOP END

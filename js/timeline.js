42e3374d7d6740f19e991ae7be96e79702bc953d
Maxime Lombard <maxime.lombard@live.com.mx>
2022-05-31 16:55:11 +0200

    update

--- js/timeline.js
+++ js/timeline.js
@@ -1,7 +1,7 @@
-/*const toTop = document.querySelector(".to-top");
+/*const toTop = document.querySelector(".carte1");
 
 window.addEventListener("scroll", () => {
-    if(window.pageXOffset > 100) {
+    if(window.pageXOffset > 500) {
         toTop.classList.add("active");
     } else {
         toTop.classList.remove("active");
